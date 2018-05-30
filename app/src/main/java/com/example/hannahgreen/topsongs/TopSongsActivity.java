2e515ee320947940675be799f1d8c3c63e031347
hannahgreen1 <hannahgreen94@icloud.com>
2018-05-30 15:12:34 +0100

    activity added

---
+++ app/src/main/java/com/example/hannahgreen/topsongs/TopSongsActivity.java
@@ -0,0 +1,13 @@
+package com.example.hannahgreen.topsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_songs);
+    }
+}
